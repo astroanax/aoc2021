795116c3d0235760a5327df527cd5d47d3bb0a0a
Rehan Tadpatri <astroanax@outlook.com>
2021-12-03 20:50:02 +0530

    day3 part2

--- solutions/3/main.py
+++ solutions/3/main.py
@@ -2,7 +2,8 @@
 
 def main():
     with open('input') as lines:
-        print(part1(lines), part2(lines))
+        print(part2(lines))
+        #print(part1(lines), part2(lines))
 
 def part1(input):
     length = None
@@ -28,8 +29,39 @@ def part1(input):
     epsilon = 2**length - 1 - gamma
 
     return gamma * epsilon
+
 def part2(input):
-    pass
+    numbers = []
+    length = None
+    for line in input:
+        if length is None:
+            length = len(line.strip())
+        numbers.append(line.strip())
+
+    numbers2 = numbers.copy()
+    for i in range(length):
+        for j in range(len(numbers)-1, -1, -1):
+            num = numbers[j]
+            if len(numbers) == 1:
+                break
+            pBit = 1 if (sum(int(n[i]) for n in numbers)/len(numbers)) >= 0.5 else 0
+            if  not pBit == int(num[i]):
+                numbers.remove(num)
+
+    #print(numbers2)
+    for i in range(length):
+        pBit = None
+        for j in range(len(numbers2)-1, -1, -1):
+            num = numbers2[j]
+            if len(numbers2) == 1:
+                break
+            if pBit is None:
+                pBit = 1 if (sum(int(n[i]) for n in numbers2)/len(numbers2)) >= 0.5 else 0
+            if  pBit == int(num[i]):
+                numbers2.remove(num)
+
+    return int('0b'+numbers[0], 2)*int('0b'+numbers2[0], 2)
+
 
 if __name__=='__main__':
     main()
